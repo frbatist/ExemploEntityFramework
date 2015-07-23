3e0aab6c019b8a739f21d2240b0e38280ac77396
frbatist <frbatist@gmail.com>
2015-07-23 14:30:02 -0300

    item venda

---
+++ ExemploEntityFramework/ExemplosDesign/Entidades/ItemVenda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploEntityFramework.ExemplosDesign
+{
+    public class ItemVenda
+    {
+        public long Id { get; set; }
+        public Venda Venda { get; set; }
+    }
+}
