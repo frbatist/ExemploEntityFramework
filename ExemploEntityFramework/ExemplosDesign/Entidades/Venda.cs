e1e6406a6489e6a3fd71d81f7ab4b4ed271d0846
frbatist <frbatist@gmail.com>
2015-07-23 14:29:17 -0300

    exemplo Open/closed

---
+++ ExemploEntityFramework/ExemplosDesign/Entidades/Venda.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploEntityFramework.ExemplosDesign
+{
+    public class Venda
+    {
+        public long Id { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+        public ICollection<ItemVenda> Itens { get; set; }
+
+        public void Processa()
+        {
+            switch (this.FormaPagamento)
+            {
+                case FormaPagamento.Dinheiro:
+                    PagamentoDinheiro();
+                    break;
+                case FormaPagamento.Cartao:
+                    PagamentoCartao();
+                    break;
+                case FormaPagamento.Cheque:
+                    PagamentoCheque();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void PagamentoCheque()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PagamentoCartao()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PagamentoDinheiro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
